f58a306ebb4f31526828c2143c2bfdc12bd7f8ee
Jeremy Tucker <jeremytucker76@gmail.com>
2022-09-26 10:25:42 -0400

    assignment complete

---
+++ Classes/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Classes
+{
+    public class Car
+    {
+        public int Year { get; set; }
+
+        public string Make { get; set; }
+        public string Model {get; set;}
+
+       
+        
+    }
+}
+
---
+++ Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyCar = new Car();
+            MyCar.Year = 2022;
+            MyCar.Make = "Lamborghini";
+            MyCar.Model = "Gallardo";
+            Console.WriteLine("test");
+            Console.WriteLine($"This is Jeremy's car, it is a {MyCar.Year} {MyCar.Make} {MyCar.Model}");
+        }
+    }
+}
+
